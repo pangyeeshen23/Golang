4ed3892a261bdcef171730f8271a8ca75f862be4
ETHAN23\pangy <Ethant Pang>
2024-02-18 15:09:37 +0800

    commit : docker with golang a simple run translator project with ui

---
+++ Docker/cmd/client/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	err := ping()
+	if err != nil {
+		fmt.Print(err.Error())
+		os.Exit(1)
+	}
+
+	err = start()
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+}
---
+++ Docker/cmd/client/request.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+var baseUrl string = "http://localhost"
+var port string = "3000"
+var requestUrl string = fmt.Sprintf("%s:%s?s=", baseUrl, port)
+
+func ping() error {
+	res, err := http.Get(requestUrl)
+	if err != nil {
+		return err
+	}
+	if res.StatusCode != http.StatusOK {
+		return errors.New("failed to establish server connection")
+	}
+
+	return nil
+}
+
+func getTranslation(msg string) string {
+	translationRequest := requestUrl + url.QueryEscape(msg)
+	response, err := http.Get(translationRequest)
+	if err != nil {
+		fmt.Printf("[error] http request failed: %s", err)
+		os.Exit(1)
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Printf("[error] failed to parse server response: %s", err)
+		os.Exit(1)
+	}
+
+	return string(body)
+}
